144554b39ca609d97f5c86dd85d2d126589c82c3
leonperez1870 <leo@parkfieldcommerce.com>
2018-12-12 10:31:41 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const createError = require('http-errors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+const shopifyRouter = require('./routes/shopify-api');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+// app.use('/shopify', shopifyRouter);
+
+app.get('/status', (req,res) =>{
+  res.send('We are Live');
+  res.end();
+});
+
+app.post('/build-product', (req, res) => {
+  res.send(req.body.response);
+  console.log('Req', req);
+});
+
+//Start Server
+app.listen(process.env.PORT || 3000, function(){
+  console.log('Server is started on port 3000');
+});
+
+module.exports = app;
